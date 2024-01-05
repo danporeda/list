b4f79de640d7116ca8c29269330283aa55878ce1
Dan <danporeda@yahoo.com>
2024-01-04 23:42:23 -0500

    refactored with Promise.all

--- Desktop/dev/javascript/node/list/index.js
+++ Desktop/dev/javascript/node/list/index.js
@@ -7,25 +7,36 @@ const util = require('util');
 // const lstat = util.promisify(fs.lstat);
 
 // Method #3
-// const { lstat } = fs.promises;
+const { lstat } = fs.promises;
 
-fs.readdir(process.cwd(), (err, filenames) => {
+fs.readdir(process.cwd(), async (err, filenames) => {
   if (err) {
     console.log(err);
   }
 
+  const statPromises = filenames.map(filename => {
+    return lstat(filename);
+  })
+
+  const allStats = await Promise.all(statPromises);
+
+  for (let stats of allStats) {
+    const index = allStats.indexOf(stats);
+
+    console.log(filenames[index], stats.isFile());
+  };
 });
 
 // Method #1
-const lstat = filename => {
-  return new Promise((resolve, reject) => {
-    fs.lstat(filename, (err, stats) => {
-      if (err) {
-        reject(err);
-      }
+// const lstat = filename => {
+//   return new Promise((resolve, reject) => {
+//     fs.lstat(filename, (err, stats) => {
+//       if (err) {
+//         reject(err);
+//       }
 
-      resolve(stats);
-    });
-  });
-};
+//       resolve(stats);
+//     });
+//   });
+// };
 
