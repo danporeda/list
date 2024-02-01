2f672a86bf5afbf16ba28438284d165e0f847d01
Dan <danporeda@yahoo.com>
2024-02-01 16:51:33 -0500

    included chai.expect into test.html

---
+++ Desktop/movies/test/autocomplete.test.js
@@ -0,0 +1,19 @@
+it('Shows an autocomplete', () => {
+  createAutoComplete({
+    root: document.querySelector('#target'),
+    fetchData() {
+      return [
+        { Title: 'Avengers'},
+        { Title: 'Butthole'},
+        { Title: 'Cunt Tut'}
+      ];
+    },
+    renderOption(movie) {
+      return movie.Title;
+    }
+  });
+
+  const dropdown = document.querySelector('.dropdown');
+
+  expect(dropdown.className).not.to.include('is-active');
+});
