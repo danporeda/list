4f9b4505d5bbf71cc6238dddae70bf95d3af3de7
Dan <danporeda@yahoo.com>
2024-02-03 23:30:37 -0500

    new test to solve undefined chai error

--- Desktop/movies/test/autocomplete.test.js
+++ Desktop/movies/test/autocomplete.test.js
@@ -1,3 +1,4 @@
+
 it('Shows an autocomplete', () => {
   createAutoComplete({
     root: document.querySelector('#target'),
@@ -16,8 +17,12 @@ it('Shows an autocomplete', () => {
   const dropdown = document.querySelector('.dropdown');
   
   expect(dropdown.className).not.to.include('is-active');
-
-  const dropdownMenu = document.querySelector('#target');
-
-  expect(dropdownMenu.className).mot.to.include('is-active');
 });
+
+it('Should do a thing', () => {
+  const ring = [1, 2, 3, 4, 5];
+
+  const rando = ring[Math.floor(Math.random() * 5)]
+
+  expect(rando).to.be.lessThan(5);
+})
